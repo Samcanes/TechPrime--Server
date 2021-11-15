a80d1097c9d123d11cc5d346bca0fe920012bf33
Irtesam Virani <irtesam.virani@gmail.com>
2021-11-15 23:17:27 +0530

    added sum of all

--- controllers/project.controllers.js
+++ controllers/project.controllers.js
@@ -15,7 +15,7 @@ exports.createProject = (req, res) => {
     location,
     createdBy,
     updatedAt,
-    status
+    status,
   } = req.body;
 
   console.log(name);
@@ -31,7 +31,7 @@ exports.createProject = (req, res) => {
     department,
     location,
     createdBy: req.user._id,
-    status
+    status,
   });
 
   project.save((error, project) => {
@@ -45,18 +45,21 @@ exports.createProject = (req, res) => {
 exports.updateProject = (req, res) => {
   console.log(req.body);
   // res.status(200).json({ file: req.files, body: req.body });
-  const {
-    _id,
-    status
-  } = req.body;
+  const { _id, status } = req.body;
 
   console.log(status);
 
-  let project = projectModels.findOneAndUpdate({ _id: _id }, { status: status }, {
-    new: true
-  },
+  let project = projectModels.findOneAndUpdate(
+    { _id: _id },
+    { status: status },
+    {
+      new: true,
+    },
     (error, project) => {
-      if (error) return res.status(400).json({ message: "Failed to update Status", error });
+      if (error)
+        return res
+          .status(400)
+          .json({ message: "Failed to update Status", error });
       if (project) {
         res.status(201).json({ message: "Status Updated", project });
       }
@@ -65,88 +68,89 @@ exports.updateProject = (req, res) => {
 };
 
 exports.readProject = (req, res) => {
-  userId = req.user._id,
-    projectModels.find({ createdBy: userId },
-      (error, project) => {
-        if (error) return res.status(400).json({ message: "Failed to recive Project", error });
-        if (project) {
-          res.status(201).json({ message: "Projects recived", projectData: project });
-        }
+  (userId = req.user._id),
+    projectModels.find({ createdBy: userId }, (error, project) => {
+      if (error)
+        return res
+          .status(400)
+          .json({ message: "Failed to recive Project", error });
+      if (project) {
+        res
+          .status(201)
+          .json({ message: "Projects recived", projectData: project });
       }
-    );
+    });
 };
 
 exports.readAllProject = (req, res) => {
-  projectModels.find({},
-    (error, project) => {
-      if (error) return res.status(400).json(
-        {
-          message: "Failed to recive Projects",
-          error
-        });
-      if (project) {
-        res.status(201).json({
-          message: "Projects recived",
-          projectData: project
-        });
-      }
+  projectModels.find({}, (error, project) => {
+    if (error)
+      return res.status(400).json({
+        message: "Failed to recive Projects",
+        error,
+      });
+    if (project) {
+      res.status(201).json({
+        message: "Projects recived",
+        projectData: project,
+      });
     }
-  );
+  });
 };
 
 exports.readGraphCounters = (req, res) => {
-  projectModels.aggregate([
-    { "$match": {} },
-    {
-      "$group": {
-        _id: { department: "$department", "status": "$status" },
+  projectModels
+    .aggregate([
+      { $match: {} },
+      {
+        $group: {
+          _id: { department: "$department", status: "$status" },
 
-        total: { $sum: 1 }
-
-      },
-    },
-     {
-      "$group": {
-        _id: "$_id.department",
-        updates: {
-
-          $push: {
-            "status": "$_id.status",
-            total: { $sum: "$total" }
-          }
+          total: { $sum: 1 },
         },
       },
-    }
-  ])
+      {
+        $group: {
+          _id: "$_id.department",
+          updates: {
+            $push: {
+              status: "$_id.status",
+              total: { $sum: "$total" },
+            },
+          },
+          sum: { $sum: "$total" },
+        },
+      },
+    ])
     .exec((error, obj) => {
       // res.status(201).json({ error, obj })
-      
-      if (error) return res.status(400).json(
-            {
-              message: "Failed to recive Projects", 
-            error 
-          });
-          if (obj) { 
-            res.status(201).json({ obj })
-          }
-    })
+
+      if (error)
+        return res.status(400).json({
+          message: "Failed to recive Projects",
+          error,
+        });
+      if (obj) {
+        res.status(201).json({ obj });
+      }
+    });
 
   // projectModels.find({},
 
   // (error, project) => {
   //   if (error) return res.status(400).json(
   //     {
-  //       message: "Failed to recive Projects", 
-  //     error 
+  //       message: "Failed to recive Projects",
+  //     error
   //   });
-  //   if (project) { 
+  //   if (project) {
   //     const projectDataRes= project.map(
   //       element => {
   //         return {name: element.department}
   //       });
   //     // console.log(project)
   //     res.status(201).json({
-  //       message: "Projects recived", 
+  //       message: "Projects recived",
   //       projectData: projectDataRes
   //      });
   //   }
